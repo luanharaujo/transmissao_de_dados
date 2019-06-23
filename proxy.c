18c7815d3b9eff10be4cacfad0106bc0b10a4652
Luan Haickel Araujo <luanharaujo@gmail.com>
2019-06-23 13:50:52 -0300

    cache funcionando

--- proxy.c
+++ proxy.c
@@ -26,31 +26,52 @@
 //************************
 //		definições
 //************************
+#define DEBUG 1
+
 #define	SA	struct sockaddr //estrutura de endereços de sockets
-#define TAM_MAX 100000 //tamanho maximo de caracteres da mensagem
+#define TAM_MAX 1000000 //tamanho maximo de caracteres da mensagem
 #define SERV_PORT 8080 //porta usada para comunicar com o navegador
-#define LISTENQ 5 //quantidade de conexões simutaneas aceitas pelo servidor até que o processo filho seja criado 
+#define LISTENQ 0 //quantidade de conexões simutaneas aceitas pelo servidor até que o processo filho seja criado 
+
+#define WHITELIST 1
+#define BLACKLIST -1
+#define NO_INFO 0
 
 //************************
 //declarações das funções
 //************************
-int proxy(int sockfd);
+int proxy(int sockfd, int cache);
 int regras(char* requisicao);
-int verifica_cache(char* requisicao);
+int verifica_cache(char* requisicao, char* identificador);
 int ler_requisicao(char* requisicao, int sockfd, char * nome_host);
 int conecta_servidor(char* nome_host);
 void enviar_requisicao(int sockfd, char * requisicao, int tam_req);
 void repassar_objetos(int sockfd_server, int sockfd_navegador);
+void criar_cache(int sockfd,int numero_cache, char* identificador);
+void cache2navegador(int numero_cache, int sockfd_navegador);
 
 //************************
 //			MAIN
 //************************
-int main()
+int main(int argc, char const *argv[])
 {
 	int					listenfd, connfd;
 	pid_t				childpid;
 	socklen_t			clilen;
 	struct sockaddr_in	cliaddr, servaddr;
+	int cache = 0;
+
+	//analizando as chaves para ativar ou nao o modo cache
+	if((argc > 1) && (strcmp("-c",argv[1])==0))
+	{
+		printf("Modo cache ativado\n");
+		cache = 1;
+	}
+	else
+	{
+		printf("Modo cache desativado\n");
+		cache = 0;
+	}
 
 	//abrindo o socket de rede internet (IPV4) do tipo stream (TCP)
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -77,49 +98,97 @@ int main()
 		//criando um processo filho para cuidar da requisição
 		if ( (childpid = fork()) == 0) {	
 			close(listenfd);//para de aceitar conexão, pois o processo pai que cuida das conexoes
-			proxy(connfd);//trata a requisição
+			proxy(connfd, cache);//trata a requisição
 			exit(0);//encerra o processo filho
 		}
 		close(connfd);//pai, fecha a conexao que será tratada pelo filho
+		if(cache)
+		{
+			//tempo necessario para que a lista de cache seja
+			//verificada e atualizanda sem que outra requisição interrompa
+			sleep(1);
+		}
 	}
 
 	//testando se consegui encerrar o programa corretamente
-	printf("passei aqui\n");
+	//para isso preciso apreder a capturar o crontrol+c
+	if(DEBUG) printf("Programa encerrado corretamente\n");
 	return 0;//retornando zero para o sistema operacional para indicar que o programa encerrou corretamente
-}	//porém a primcipio o programa não deve encerrar, haha
+}	
 
 //************************
 //assinaturas das funções
 //************************
 
 //função que ferifica a lista preta e a lista branca
-//retorna 1 caso o site seja liberado e 0 caso seja bloqueado
+//retorna 1 caso o site esteja na whitelist, -1 caso
+//esteja na blacklist e 0 caso não esteja em nenhuma das listas
 int regras(char* requisicao)
 {
-	return 1;
+	return (WHITELIST);
 }
 
 //verifica se a pagina já encontrasse no cache
-//retorna 1 caso já esteja no cache e 0 caso não esteja
-int verifica_cache(char* requisicao)
+//retorna o numero do arquivo cache caso já esteja 
+//no cache e um valor negativo correspondete ao 
+//proximo numero disponiovel caso não esteja
+int verifica_cache(char* requisicao, char* identificador)
 {
-	char identificador[300];
+	char line[300];
+	int numero_arquivo_cache;
+	char aux;
+	int i;
+	FILE *fp;
+	
+
+	//abrindo o arquivo ande esta indexado os arquivos de cache
+	fp = fopen("cache/cache_list.txt","r");
+
+	if(!fp)//caso o arquivo ainda não exista siguinifica que ainda não existe nenhum cache
+	{
+		return(0);
+	}
 
 	//recebendo a primeira linha da requisição que escolhi para identificar as entradas do cache
 	sscanf(requisicao, "%[^\n]s", identificador);
 
-	return 0;
+	//buscando se a requisiçao já existe no cache
+	numero_arquivo_cache = 1;
+	do//percorrendo cada linha do arquivo
+	{
+		i = 0;
+		do//percorrendo as letras de cada linha
+		{
+			fscanf(fp,"%c",&line[i++]);
+		}while((line[i-1] != EOF) && (line[i-1] != '\n'));//verificando se encontrou o fim da linha ou do arquivo
+		aux = line[i-1];//cuidando para nao perder o EOF
+		line[i-1] = '\0';//arrumando o fim da string
+		if(strcmp(line,identificador)==0)//checando esta linha
+		{
+			//caso encontre: fecha o arquivo, retorna o valor, encerra a rotina, escova os dentes, apaga a luz e vai dormir
+			fclose(fp);
+			return(numero_arquivo_cache);
+		}
+		numero_arquivo_cache++;
+	}while(aux != EOF);
+
+	//fecehndo o arquivo
+	fclose(fp);
+	//retorna zero para indicar que nao encontrou
+	return (-numero_arquivo_cache);
 }
 
 //recebe uma requisicao do navegador e retorna na variável requisicao
 //retrona o tamanho da requisição para faciliar o envio dela para o servidor posteriormente
 int ler_requisicao(char* requisicao, int sockfd, char * nome_host)
 {
-	//variavel auxiliar para nao perder o ponteiro inicial
 	char *aux = requisicao;
+	//variavel auxiliar para nao perder o ponteiro inicial
+	
 	//variaveis auxiliares para identificao do fim da requisicao
 	char *inicio_linha = requisicao;
 	char *fim_linha;
+
 	
 	int tamanho = 0;//guardar o tamnho da requisição
 	int achou_host = 0;//flag para identificar o conhecimento ou não do nome do host
@@ -152,41 +221,73 @@ int ler_requisicao(char* requisicao, int sockfd, char * nome_host)
 }
 
 //função que trata cada requisição feita pelo navegador
-int proxy(int socket_cliente)
+int proxy(int socket_cliente, int cache)
 {
-	char objeto[TAM_MAX];
-	char requisicao[TAM_MAX];
-	int tam_req;
-	int socket_servidor;
-	char nome_host[300];
+	static char objeto[TAM_MAX];
+	static char requisicao[TAM_MAX];
+	static int tam_req;
+	static int socket_servidor;
+	static char nome_host[300];
+	static int numero_cache;
+	static char identificador_requisicao[300];
 
+	//zerado todas as strings
+	bzero(objeto,sizeof(objeto));
+	bzero(nome_host,sizeof(nome_host));
+	bzero(requisicao,sizeof(requisicao));
+	bzero(identificador_requisicao,sizeof(identificador_requisicao));
 	
 	tam_req = ler_requisicao(requisicao, socket_cliente, nome_host);
-	printf("%s\n", nome_host);
+	if(DEBUG) printf("Nova requisição recebina: %s\n", nome_host);
 	//log();
-	if(regras(requisicao))
+	switch(regras(requisicao)) 
 	{
-		//passou nas regras para conexão
-		if(!verifica_cache(nome_host))
-		{
-			//ler do servidor e criar cache
-			socket_servidor = conecta_servidor(nome_host);
-			enviar_requisicao(socket_servidor, requisicao, tam_req);
-			repassar_objetos(socket_servidor, socket_cliente);
-	
-		}
-		else
-		{
-			//repassar do cache
-			//obejeto = ler_cache(requisicao);
-		}
+		case WHITELIST :
+			if(cache)
+			{
+				//verificando se a resposta para esta requisição já esta no cache
+				numero_cache = verifica_cache(requisicao,identificador_requisicao);
+				if(DEBUG) printf("\nNumero cache: %d\n", numero_cache);
+				if(numero_cache < 0)//caso ainda não esteja no cache
+				{
+					//criar cache
+					numero_cache *= -1;//tornarndo o novo numero de cache positivo
+
+					//conecta no servidor
+					socket_servidor = conecta_servidor(nome_host);
+					//envia a requisição para o servidor
+					enviar_requisicao(socket_servidor, requisicao, tam_req);
+					//recebe o objeto e guarda em um cache
+					criar_cache(socket_servidor,numero_cache, identificador_requisicao);
+					//passa o conteudo do cache para o navegardor
+					cache2navegador(numero_cache, socket_cliente);
+				}
+				else
+				{
+					//repassar do cache
+					if(DEBUG) printf("CACHE DESTA REQUISICAO EXISTE\n");
+					cache2navegador(numero_cache, socket_cliente);
+				}
+			}
+			else//caso o programa esteja rodando sem cache
+			{
+				//conecta no servidor
+				socket_servidor = conecta_servidor(nome_host);
+				//envia a requisição para o servidor
+				enviar_requisicao(socket_servidor, requisicao, tam_req);
+				//passar mensagem direto do servidor para o navegador
+				repassar_objetos(socket_servidor, socket_cliente);
+			}
+		break;
+		case BLACKLIST :
+			//gerar entrada no log
+			//enviar mensagem de bloqueio ao navegador
+		break;
+		case NO_INFO :
+			//verificar lista de termos proibidos
+		break;
 	}
-	else
-	{
-		//reprovou nas regras para conexão
-		//mensagem de erro apra o navegador
-	}
-	
+	if(DEBUG) printf("Tratamento da requisição encerrado: %s\n", nome_host);
 	return 0;
 }
 
@@ -212,10 +313,9 @@ int conecta_servidor(char* host_name)
 	//obtendo endereço ip a partir do host name
 	getaddrinfo(host_name, "80", &hints, &result);
 
-	//preciso entender esta parte melhor, ainda nao comprendi totalmetne
+	//percorrendo a lista de endereços IPs fornecida pelo DNS
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
-       sockfd = socket(rp->ai_family, rp->ai_socktype,
-                    rp->ai_protocol);
+       sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
        if (sockfd == -1)
            continue;
 
@@ -235,21 +335,79 @@ int conecta_servidor(char* host_name)
 //função que envia requisição para o servidor
 void enviar_requisicao(int sockfd, char *requisicao, int tam_req)
 {
+	
 	write(sockfd, requisicao, tam_req);
-
+	
 	return;
 }
 
+//legace: usada antes da implementação do cache
 //função que repassa objeto do servidor para o navegador
-//e cria o cache
 void repassar_objetos(int sockfd_server, int sockfd_navegador)
 {
 	char temp;
 
 	while(read(sockfd_server,&temp,1)>0){
-		//printf("%c", temp);
+		//if(DEBUG) printf("%c", temp);
 		write(sockfd_navegador,&temp,sizeof(temp));	
 	}
+	
+	return;
+}
+
+//função que repassa objeto do cache para o navegador
+void cache2navegador(int numero_cache, int sockfd_navegador)
+{
+	char temp;
+	FILE* fp;
+	char arquivo_cache[50];
+
+	//abrindo o cache
+	sprintf(arquivo_cache,"cache/cache_%d.txt",numero_cache);
+	fp = fopen(arquivo_cache,"r");
+
+
+	while(fscanf(fp,"%c",&temp)>0){
+		//if(DEBUG) printf("%c", temp);
+		write(sockfd_navegador,&temp,sizeof(temp));	
+	}
+	
+	//fechando o arquivo
+	fclose(fp);
+
+	return;
+}
+
+//funcao que cria um arquivo cache do site requisitado
+//e atualiza o arquivo com as listas de cache
+void criar_cache(int sockfd,int numero_cache, char *identificador)
+{
+	char temp;
+	FILE *fp;
+	char arquivo_cache[30];
+
+	//atualizando a lista de arquivos
+	if(DEBUG) printf("Atualizando a lista de cache\n");
+	fp = fopen("cache/cache_list.txt","a");
+	fprintf(fp, "\n%s", identificador);
+	fclose(fp);
+	if(DEBUG) printf("Lista de chache atualizada\n");
+
+
+	if(DEBUG) printf("Criando arquivo cache!\n");
+	//criando o novo arquivo cache
+	sprintf(arquivo_cache,"cache/cache_%d.txt",numero_cache);
+	fp = fopen(arquivo_cache,"w");
+
+	//gravando no arquivo 
+	while(read(sockfd,&temp,1)>0){
+		fprintf(fp, "%c", temp);
+	}
+
+	//fechando o arquivo
+	fclose(fp);
+
+	if(DEBUG) printf("Arquivo cache criado!\n");
 	
 	return;
 }
